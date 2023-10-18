6fbba2ef90d14f4b895c95492dbe9f7a4542ba0e
richard829 <richardpulido2001@gmail.com>
2023-10-18 10:37:00 -0500

    change book system from function based to a class based system

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const myLibrary = [];
 const library = document.querySelector('.container');
 const btn = document.querySelector('#btn');
 const modal = document.querySelector('.new-book-form');
@@ -11,59 +10,76 @@ btn.addEventListener('click' , (event) => {
     modal.showModal();
 });
 
-function Book(title, author, read) {
-    this.title = title
-    this.author = author
-    this.read = read
-    this.index = myLibrary.length
+// class approach //
+
+class Bookclass {
+    static library = []
+
+    constructor(title, author, read){
+        this.title = title
+        this.author = author
+        this.read = read
+        this.index = Bookclass.library.length
+    }
+
+    addBook (title, author, read) {
+        let libro = new Bookclass(title,author,read);
+        Bookclass.library.push(libro);
+    }
+
+    displayBooks () {
+        let html = ''
+        for (let i=0; i<Bookclass.library.length; i++) {
+            html += `<div>`
+            html += `<img class='bookImg' src='./img/book-covers-big-2019101610.jpg' alt='book Image'>`
+            html += `<p>Title: ${Bookclass.library[i].title}</p>`;
+            html += `<p>Author: ${Bookclass.library[i].author}</p>`;
+            html += `<p>Read: ${Bookclass.library[i].read}</p>`;
+            html += `<button class='btn btn-remove' onclick='Bookclass.prototype.removeBook(${Bookclass.library[i].index})'>Remove Book</button>`;
+            html += `</div>`;
+        }
+        library.innerHTML = html;
+    }
+
+    removeBook(bookIndex) {
+        let filter = Bookclass.library.filter((item,index) => {
+            if(bookIndex === item.index) {
+                Bookclass.library.splice(index,1);
+                Bookclass.prototype.displayBooks();
+            }
+        })
+    }
+
+    resetForm(){
+    title.value = ''; author.value = ''; read.value = ''
+    }
+
+    init() {
+      this.displayBooks();  
+    }
+
+
 }
 
-function addBook (title, author, read) {
-    let libro = new Book(title,author,read);
-    myLibrary.push(libro);
-}
+
+Bookclass.prototype.addBook(`title.value`, `author.value`, `read.value`);
+Bookclass.prototype.addBook(`title.value`, `author.value`, `read.value`);
+Bookclass.prototype.addBook(`title.value`, `author.value`, `read.value`);
+
+
+Bookclass.prototype.init();
+
+
 
 formBtn.addEventListener('click' , event => {
     event.preventDefault();
-    // if(title.value && author.value && read.value) 
-    // {
-    addBook(title.value, author.value, read.value);
-    displayBooks();
-    resetForm();
-    // }
+        if(title.value && author.value && read.value) 
+    {
+        Bookclass.prototype.addBook(title.value, author.value, read.value);
+        Bookclass.prototype.displayBooks();
+        Bookclass.prototype.resetForm();
+    }
+    else return
 })
 
-function removeBook(bookIndex) {
-    let filter = myLibrary.filter((item,index) => {
-        if(bookIndex === item.index) {
-            myLibrary.splice(index,1);
-            displayBooks();
-        }
-    })
-};
-
-function displayBooks () {
-    let html = ''
-    for (i=0; i<myLibrary.length; i++) {
-        html += `<div>`
-        html += `<img class='bookImg' src='./img/book-covers-big-2019101610.jpg' alt='book Image'>`
-        html += `<p>Title: ${myLibrary[i].title}</p>`;
-        html += `<p>Author: ${myLibrary[i].author}</p>`;
-        html += `<p>Read: ${myLibrary[i].read}</p>`;
-        html += `<button class='btn btn-remove' onclick='removeBook(${myLibrary[i].index})'>Remove Book</button>`;
-        html += `</div>`;
-    }
-    library.innerHTML = html;
-}
-
-function resetForm(){
-    title.value = ''; author.value = ''; read.value = ''
-}
-
-addBook(`title.value`, `author.value`, `read.value`);
-addBook(`title.value`, `author.value`, `read.value`);
-addBook(`title.value`, `author.value`, `read.value`);
-displayBooks();
-
-
-
+// notas para luyego, refactorizar para que los elementos del DOM sean parte de la clase //
